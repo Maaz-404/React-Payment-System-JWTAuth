e7fa9b7159ffab44a38beb35378c18a0f85b113a
maaz4 <maazmukhtar966@gmail.com>
2021-07-07 12:17:28 +0500

    Update UI for admin approval

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -24,50 +24,45 @@ const BoardAdmin = () => {
   }, []);
 
   return (
-    <div className="container">
+    <div className="container-fluid">
       <header className="jumbotron">
         <h3>{content}</h3>
       </header>
-      <div class="align-items-between">
-        Current Balance
-        Total Received
-        Commission/Earned
-      </div>
 
-            <div class="row">
-            <div class="col-sm-6">
+        <div class="row">
+          
                 <div class="card">
                 <div class="card-body">
                     <h4 class="card-text">Total Received from Donations</h4>
                 </div>
                 </div>
-            </div>
+         
             
-            <div class="col-sm-4">
-                <div class="card">
+            
+                <div class="card ml-4">
                 <div class="card-body">
                     <h5 class="card-title">Current Balance </h5>
                 </div>
                 </div>
+            
+        </div>
+            
+            <div class="row align-items-right">
+                <h3 class="card-title">Transactions </h3>
             </div>
-            <div class="col-sm-6">
+            
+            <div class="col-lg">
                 <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Donation from : </h5>
-                    <p class="card-text">Amount Donated : </p>
-                </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Commission Earned on Transaction :   </h5>
+                    
+                    <h5 class="card-text">Amount Donated : </h5>
+                    <p class="card-title">Donation By : </p>
+                    <h5 class="card-title">Commission To be Earned on Transaction :   </h5>
                     <button class="btn btn-success">Approve</button>
                     <button class="btn btn-dark mt-2">Decline / Cancel</button>
                 </div>
                 </div>
             </div>
-        </div>
       </div>
     
   );
