1e68d925604cfd4b2fa3a489362c1b0e5e461f68
maaz4 <maazmukhtar966@gmail.com>
2021-07-08 12:03:21 +0500

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import * as serviceWorker from "./serviceWorker";
-// Import Stripe api
-// import { loadStripe } from "@stripe/stripe-js";
-// import { Elements } from "@stripe/react-stripe-js";
-// 
-// Load Stripe api
-// const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 ReactDOM.render(
     <BrowserRouter>
